db9fa24bb98df8dd95e8a5f9d76cff56fe021074
Tong Dang Khoa <khoatd584@gmail.com>
2022-11-16 13:44:18 +0700

    up

---
+++ JS/app.js
@@ -0,0 +1,35 @@
+// Slick slider
+$(document).ready(function() {
+    $('.shop').slick({
+
+        infinite: false,
+        speed: 300,
+        slidesToShow: 4,
+        slidesToScroll: 2,
+        responsive: [{
+                breakpoint: 1024,
+                settings: {
+                    slidesToShow: 3,
+                    slidesToScroll: 2,
+                }
+            },
+            {
+                breakpoint: 600,
+                settings: {
+                    slidesToShow: 2,
+                    slidesToScroll: 2
+                }
+            },
+            {
+                breakpoint: 480,
+                settings: {
+                    slidesToShow: 1,
+                    slidesToScroll: 1
+                }
+            }
+        ],
+        prevArrow: `<button type='button' class='slick-prev arrow'><ion-icon name="chevron-back-outline"></ion-icon></button>`,
+        nextArrow: `<button type='button' class='slick-next arrow'><ion-icon name="chevron-forward-outline"></ion-icon></button>`
+    });
+});
+// End Slider
